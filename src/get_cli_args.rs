ab24e5989853428cbd2c5755d5abc3a5bdcaaed7
Greg Kapka <greg@oraclize.it>
2019-12-25 23:50:53 +0000

    feat(usage): adds cli arg getter & usage info

--- src/get_cli_args.rs
+++ src/get_cli_args.rs
@@ -1,8 +1,5 @@
 use docopt::Docopt;
-use std::{
-    path::Path,
-    fs::read_to_string,
-};
+use bitcoin::network::constants::Network as BtcNetwork;
 use crate::{
     types::Result,
     errors::AppError,
@@ -13,12 +10,29 @@ use crate::{
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct CliArgs {
     pub cmd_makeTx: bool,
+    pub flag_network: String,
+    pub flag_keyfile: String,
     pub arg_utxo_index: usize,
     pub arg_btc_block_hash: String,
 }
 
+pub fn get_network_from_cli_arg(network_cli_arg: &String) -> BtcNetwork {
+    info!("✔ Getting network from cli-arg: '{}'", network_cli_arg);
+    match &network_cli_arg[..] {
+        "Testnet" => {
+            info!("✔ Using network: 'Testnet'");
+            BtcNetwork::Testnet
+        }
+        _ => {
+            info!("✔ Using network: 'Bitcoin'");
+            BtcNetwork::Bitcoin
+        }
+    }
+}
+
+
 pub fn get_cli_args() -> Result<CliArgs> {
-    trace!("✔ Getting CLI args...");
+    info!("✔ Getting CLI args...");
     match Docopt::new(USAGE_INFO) 
         .and_then(|d| d.deserialize()) {
             Ok(cli_args) => Ok(cli_args),
--- src/usage_info.rs
+++ src/usage_info.rs
@@ -11,18 +11,24 @@ A maker of BTC transactions!
 ❍ Usage ❍
 
 Usage:  btc-tx-maker --help
-        btc-tx-maker makeTx <btc_block_hash> <utxo_index>
+        btc-tx-maker makeTx <btc-block-hash> <utxo-index> [--keyfile=<path>] [--network=<string>]
 
 Commands:
 
     makeTx              ❍ Do the thing!
 
-    <btc_block_hash>    ❍ The hash of the block containing the UTXO to spend.
+    <btc-block-hash>    ❍ The hash of the block containing the UTXO to spend.
 
-    <utxo_index>        ❍ The index of the UTXO in the block.
+    <utxo-index>        ❍ The index of the UTXO in the block.
 
 Options:
 
     --help               ❍ Show this message.
+
+    --network=<string>   ❍ Btc network: Either `Bitcoin` or `Testnet`.
+                           [default: Bitcoin]
+
+    --keyfile=<path>     ❍ Path to GPG-encrypted BTC private key hex string.
+                           [default: ./encrypted-btc-private-key.gpg]
 
 ";
