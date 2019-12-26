ba01e8d8796eb8a52b73eadfbef437feff6a1295
Greg Kapka <greg@oraclize.it>
2019-12-26 14:36:07 +0000

    feat(types): adds btc txs type for convenience

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,7 @@
 use std::result;
 use crate::errors::AppError;
+use bitcoin::blockdata::transaction::Transaction as BtcTransaction;
 
 pub type Bytes = Vec<u8>;
+pub type BtcTransactions = Vec<BtcTransaction>;
 pub type Result<T> = result::Result<T, AppError>;
