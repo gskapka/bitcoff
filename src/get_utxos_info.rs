3313e54efe599bfda3e6baff74354e590c07ea8a
Greg Kapka <greg@oraclize.it>
2019-12-26 23:49:40 +0000

    fix(utxo-info): fix wrong ordering of URL params & update fxn names

--- src/get_utxos_info.rs
+++ src/get_utxos_info.rs
@@ -9,11 +9,11 @@ use crate::{
     utils::make_api_call,
 };
 
-fn get_utxo_list_json_string(
+fn get_utxo_info_json_string(
     address: &String,
     api_endpoint: &String,
 ) -> Result<String> {
-    info!("✔ Getting UTXO list for address: {}", address);
+    info!("✔ Getting UTXO info for address: {}", address);
     make_api_call(
         &format!("{}address/{}/utxo", api_endpoint, address)[..],
         "✘ Error getting UTXO list",
@@ -32,9 +32,9 @@ fn parse_utxo_list_json_string(
 
 pub fn get_utxos_info_and_add_to_state(state: State) -> Result<State> {
     info!("✔ Getting UTXOs info and adding to state...");
-    get_utxo_list_json_string(
-        &state.api_endpoint,
+    get_utxo_info_json_string(
         &state.get_btc_address()?,
+        &state.api_endpoint,
     )
         .and_then(parse_utxo_list_json_string)
         .and_then(|utxos_info| state.add_utxos_info(utxos_info))
@@ -50,7 +50,7 @@ mod tests {
 
     #[test]
     fn should_get_utxo_list_json_string() {
-        if let Err(e) = get_utxo_list_json_string(
+        if let Err(e) = get_utxo_info_json_string(
             &SAMPLE_TARGET_BTC_ADDRESS.to_string(),
             &SAMPLE_TESTNET_ENDPOINT.to_string(),
         ) {
@@ -60,7 +60,7 @@ mod tests {
 
     #[test]
     fn should_parse_utxo_list_json_string() {
-        let utxo_list_json_string = get_utxo_list_json_string(
+        let utxo_list_json_string = get_utxo_info_json_string(
             &SAMPLE_TARGET_BTC_ADDRESS.to_string(),
             &SAMPLE_TESTNET_ENDPOINT.to_string(),
         ).unwrap();
